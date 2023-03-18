8ab53c5b569505441e85ab1c7421e3d58bb02338
Eugen Ciur <eugen@papermerge.com>
2023-03-18 09:11:16 +0100

    minor input fixes

--- setappversion/main.py
+++ setappversion/main.py
@@ -9,9 +9,9 @@ from setappversion.utils import (
 
 def main():
     # path to helmchart file e.g. "charts/docs/Chart.yaml"
-    file_path = os.environ["INPUT_FILE"]
+    file_path = os.environ["INPUT_HELMCHART_FILE"]
     # new app version to be set in helmchart e.g. "1.5.6"
-    new_version = os.environ["NEW_APP_VERSION"]
+    new_version = os.environ["INPUT_NEW_APP_VERSION"]
 
     content = get_file_content(file_path)
     new_content = replace_ver(content, str(new_version))
